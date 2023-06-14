b6e4943e19561e3fcce36fdb825fe7b8c95b9b1d
Mimi George <mromany@hotmail.com>
2023-06-14 15:36:16 -0500

    non final commit

--- PyBank/main.py
+++ PyBank/main.py
@@ -2,39 +2,69 @@
 import csv
 import os
 
-# Path to collect data from the Resources folder
+# Path to collect data from the Resources folder and export results
 pybank_csv = os.path.join("Resources", "budget_data.csv")
+pybank_export = os.path.join("Analysis", "Financial_Analysis.txt")
 
-# Lists to store data
-Date = []
-ProfitLosses = []
-MonthsTotal = []
-ProfitLossesNetTotal = []
-ProfitLossesChanges = []
-GreatestProfitIncrease = []
-GreatestProfitDecrease = []
-
-# Open and read csv
+# Open and read csv including the header
 with open(pybank_csv) as csv_file:
-        csv_reader = csv.reader(csv_file, delimiter=",")
+        csv_reader = csv.reader(csv_file)
         csv_header = next(csv_reader)
+
+        # Create lists and variables to store data
+        MonthsTotal = 0
+        ProfitLossesNetTotal = 0
+        InitialChange = 0
+        ProfitLossesChanges = []
+        MonthName = []
+        HighestProfitIncrease = 0
+        
         for row in csv_reader:
                  
                  # Populate the Date list and determine the total number of months
-                 Date.append(row[0])
-                 MonthsTotal = len(Date)
-                 print (f"Total months: {str(MonthsTotal)}")
+                 MonthsTotal += 1
                  
-                 # Populate the Profit/losses list and determine the net total
-                 ProfitLosses.append(row[1])
-                 ProfitLossesNetTotal = sum(int(ProfitLosses))
-                 print (f"Total: {float(ProfitLossesNetTotal)}")
+                 # Determine the net total
+                 ProfitLossesNetTotal += int(row[1])
 
-                 # Determine the greatest profit increase
-                 GreatestProfitIncrease = max(ProfitLosses)
+                 # Determine changes in profit/losses and add data to a list
+                 MonthlyDiff = int(row[1]) - InitialChange
+                 ProfitLossesChanges.append(MonthlyDiff)
+                 MonthName = [row[0]]
+                 InitialChange = int(row[1]) 
+
+                 # Determine the average of those changes
+                 MonthlyChangesAverage = sum(ProfitLossesChanges) / len(ProfitLossesChanges) 
+
+                 # Determine the greatest increase
+                 #for changes in ProfitLossesChanges:
+                         #if (changes > HighestProfitIncrease):
+                                 #HighestProfitIncrease = changes
+                                 #MonthIncrease = row[0]
+                 HighestProfitIncrease = max(ProfitLossesChanges)
 
                  # Determine the greatest profit decrease
-                 GreatestProfitDecrease = min(ProfitLosses)
+                 HighestProfitDecrease = min(ProfitLossesChanges)
+
+                 # Variable to store results
+                 Output = (f"Financial Analysis\n"
+                           "----------------------------\n"
+                           f"Total Months: {MonthsTotal}\n"
+                           f"Total: ${ProfitLossesNetTotal}\n"
+                           f"Average Change: ${MonthlyChangesAverage:.2f}\n"
+                           f"Greatest Increase in Profits: {MonthName} ${HighestProfitIncrease}\n"
+                           f"Greatest Decrease in Profits: {MonthName} ${HighestProfitDecrease}")
+                 
+        # Print results in terminal
+        print (Output)
+
+# Export results to the text file
+with open(pybank_export, "w") as txt_output:
+        txt_output.write(Output)
+
+                 
+
+
 
 
                  
--- PyPoll/main.py
+++ PyPoll/main.py
@@ -2,39 +2,61 @@
 import os
 import csv 
 
-# Path to collect data from the Resources folder
-pypoll_csv = os.path.join("..", "Resources", "election_data.csv")
+# Path to collect data from the Resources folder and export results
+pypoll_csv = os.path.join("Resources", "election_data.csv")
+pypoll_export = os.path.join("Analysis", "Election_Results.txt")
 
-# Lists to store data
+# Lists and variables to store data
 Candidate_list = []
 UniqueCandidatelist = []
 VotesPerCandidate = []
 PercentageOfVotes = []
+TotalVotes = 0
+HighestVotes = 0
 
-# Open and read csv
+# Open and read csv including the header
 with open(pypoll_csv) as csv_file:
-        csv_reader = csv.reader(csv_file, delimiter=",")
-        for row in csv_reader:
-                # Determine the total number of votes cast
-                 TotalVotes = len(row[0])
-                 print (f"Total votes: {str(TotalVotes)}")
-        
-        # Populate the candidate list with the candidate names invluding the header
+        csv_reader = csv.reader(csv_file)
         csv_header = next(csv_reader)
+
         for row in csv_reader:
+
+                # Determine the total number of votes cast
+                TotalVotes += 1
+                
+                # Populate the candidate list with the candidate names 
                 Candidate_list.append(row[2])
                 UniqueCandidatelist = set(Candidate_list)
-
-        # Returning the total number of votes each candidate won
-        for candidate in UniqueCandidatelist:
-                VotesPerCandidate = Candidate_list.count(candidate)  
-                Winner_Candidate = max(VotesPerCandidate)
-                PercentageOfVotes = (VotesPerCandidate / TotalVotes) * 100
-
-#Printing results
-print("Election Results:", "--------------------------------", f"Total Votes: {TotalVotes}", "--------------------------------", )
-
-
                 
+                # Return the total number of votes each candidate won and percentage
+                for candidate in UniqueCandidatelist:
+                        VotesPerCandidate = Candidate_list.count(candidate)
+                        PercentageOfVotes = float(VotesPerCandidate) / float(TotalVotes) * 100
+
+                        # Determine the winner 
+                        if (VotesPerCandidate > HighestVotes):
+                                HighestVotes = VotesPerCandidate
+                                Winner = candidate        
+                        
+                        # Variable to store results
+                        Output = (f"Election Results:\n"
+                                  "--------------------------------\n"
+                                  f"Total Votes: {TotalVotes}\n"
+                                  "--------------------------------\n"
+                                  f"{candidate}: {PercentageOfVotes:.3f}% ({VotesPerCandidate})\n"
+                                  "--------------------------------\n"
+                                  f"Winner: {Winner}\n"
+                                  "--------------------------------\n")
+        
+        # Print results in terminal
+        print(Output)
+
+# Export results to the text file
+with open(pypoll_export, "w") as txt_output:
+        txt_output.write(Output)
+
+
+
+        
 
 
